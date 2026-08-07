c6e4e5b00f81f76c2b7b2b5198b13b102850d368
Kunal Kumar <kunalkr5674@gmail.com>
2026-08-07 21:24:20 +0530

    Configure deployment

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,12 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+   base: '/portfolio/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    }
-  }
+    },
+  },
 })
 
