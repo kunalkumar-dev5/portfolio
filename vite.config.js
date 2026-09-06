6c3136212830ec06671117fdd7349a93e5ee9ad1
Kunal Kumar <kunalkr5674@gmail.com>
2026-09-06 21:39:35 +0530

    fix: update base path for vercel

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 
-  base: '/portfolio/',
+  base: '/',
   server: {
     host: true,
   },
